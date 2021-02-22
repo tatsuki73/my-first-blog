ff4680dca9f4b4baddde384527447eaa291cea31
tatsuki73 <78690476+tatsuki73@users.noreply.github.com>
2021-02-22 19:40:46 +0900

    sh: execute: command not found

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,13 +155,5 @@ if not DEBUG:
     import django_heroku #追加
     django_heroku.settings(locals()) #追加
 
-DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'name',
-    'USER': 'user',
-    'PASSWORD': '',
-    'HOST': 'host',
-    'PORT': '',
-    }
-    }
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
