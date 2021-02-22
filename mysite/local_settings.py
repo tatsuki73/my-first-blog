87c866ba25010fa300a195b7bece39b0018b082a
tatsuki73 <78690476+tatsuki73@users.noreply.github.com>
2021-02-22 20:06:00 +0900

    sh: execute: command not found

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -3,6 +3,8 @@ import os
 #settings.pyからそのままコピー
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = '92vrxamb9dqxnup=%0k-70z!x#iwhk)*2#smov*f7%d2w2x63v'
+
 #settings.pyからそのままコピー
 DATABASES = {
     'default': {
